5b841dc790d31d5eb5fde2a84ba18b6fb7350e31
fencim <fencim@gmail.com>
2025-04-05 15:01:02 +0800

    setup typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import configuration from './config/configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    envFilePath: ['.dev.env'],
-    load: [configuration]
-  })],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.dev.env'],
+      load: [configuration]
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'JobsDB',
+      entities: [],
+      synchronize: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
