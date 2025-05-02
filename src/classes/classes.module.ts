bdfcff24b38ba98c5e254b2b65661370845027c4
Mohaymen Omar <mohaymenomar13@gmail.com>
2025-05-02 12:44:20 +0800

    added controller classes

--- src/classes/classes.module.ts
+++ src/classes/classes.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ClassesController } from './classes.controller';
 
-@Module({})
+@Module({
+  controllers: [ClassesController]
+})
 export class ClassesModule {}
