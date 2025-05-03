0841651608267b0267d196ed82280ee7ccd373bc
rassel <rasselmangco12345@gmail.com>
2025-05-03 23:17:38 +0800

    Modefied Controller Classes

--- src/classes/classes.controller.ts
+++ src/classes/classes.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { ClassesService } from './classes.service';
+import { CreateClassesDto } from './dto/create-classes.dto';
+import { UpdateClassesDto } from './dto/update-classes.dto';
 
 @Controller('classes')
-export class ClassesController {}
+export class ClassesController {
+    constructor(private readonly classesService: ClassesService) {}
+
+@Post()
+create(@Body() createClassesDto: CreateClassesDto) {
+  return this.classesService.create(createClassesDto);
+}
+
+@Get()
+findAll() {
+  return this.classesService.findAll();
+}
+
+@Get(':id')
+findOne(@Param('id') id: string) {
+  return this.classesService.findOne(+id);
+}
+
+@Patch(':id')
+update(@Param('id') id: string, @Body() UpdateClassesDto: UpdateClassesDto) {
+  return this.classesService.update(+id, UpdateClassesDto);
+}
+
+@Delete(':id')
+remove(@Param('id') id: string) {
+  return this.classesService.remove(+id);
+}}
