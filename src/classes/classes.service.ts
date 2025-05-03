46d866785c63627583e485e9e0642cf4bf74007c
rassel <rasselmangco12345@gmail.com>
2025-05-03 23:09:04 +0800

    added Service Classes

--- src/classes/classes.service.ts
+++ src/classes/classes.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { Classes } from './entities/classes.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateClassesDto } from './dto/create-classes.dto';
+import { UpdateClassesDto } from './dto/update-classes.dto';
 
 @Injectable()
-export class ClassesService {}
+export class ClassesService {
+    constructor(
+        @InjectRepository(Classes)
+        private classesRepository: Repository<Classes>,
+      ) {}
+    
+    
+    
+    create(createClassesDto: CreateClassesDto) {
+      return this.classesRepository.save(createClassesDto);
+    }
+  
+    findAll() {
+      return this.classesRepository.find;
+    }
+  
+    findOne(id: number) {
+      return `This action returns a #${id} teacher`;
+    }
+  
+    update(id: number, updateClassesDto: UpdateClassesDto) {
+      return `This action updates a #${id} teacher`;
+    }
+  
+    remove(id: number) {
+      return `This action removes a #${id} teacher`;
+    }
+}
