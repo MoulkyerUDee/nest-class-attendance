158a3f3d95c1d35f4c123c97fa914619a504e248
aisha-abdulmanan <abdulmanan.sc123@s.msumain.edu.ph>
2025-05-09 01:57:56 +0800

    Initialize supervisor resource files

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,6 +12,7 @@ import { ClassesModule } from './classes/classes.module';
 import { StudentsModule } from './students/students.module';
 import { Student } from './students/entities/student.entity';
 import { Classes } from './classes/entities/classes.entity';
+import { SupervisorModule } from './supervisor/supervisor.module';
 
 @Module({
   imports: [
@@ -32,7 +33,8 @@ import { Classes } from './classes/entities/classes.entity';
     UsersModule,
     TeacherModule,
     ClassesModule,
-    StudentsModule
+    StudentsModule,
+    SupervisorModule
   ],
   controllers: [AppController],
   providers: [AppService],
