1b6813a530fafb6a7cb31dc14a168bc535f6e38e
aisha-abdulmanan <abdulmanan.sc123@s.msumain.edu.ph>
2025-05-16 01:13:15 +0800

    Secure supervisor endpoints with JWT and role guards

--- src/supervisor/supervisor.controller.spec.ts
+++ src/supervisor/supervisor.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SupervisorController } from './supervisor.controller';
 import { SupervisorService } from './supervisor.service';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { Roles } from 'src/decorators/roles.decorator';
+import { Role } from 'src/enums/role.enum';
 
 describe('SupervisorController', () => {
   let controller: SupervisorController;
--- src/supervisor/supervisor.controller.ts
+++ src/supervisor/supervisor.controller.ts
@@ -1,28 +1,37 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { SupervisorService } from './supervisor.service';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { Roles } from 'src/decorators/roles.decorator';
+import { Role } from 'src/enums/role.enum';
 
 
 @Controller('supervisor')
 export class SupervisorController {
   constructor(private readonly supervisorService: SupervisorService) {}
-
- 
-
+  
+  @ApiBearerAuth()
+  @Roles(Role.Admin, Role.Supervisor)
   @Get()
   findAll() {
     return this.supervisorService.findAll();
   }
 
+  @ApiBearerAuth()
+  @Roles(Role.Admin, Role.Supervisor)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.supervisorService.findOne(+id);
   }
 
+  @ApiBearerAuth()
+  @Roles(Role.Admin)                                // only admin can delete
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.supervisorService.remove(+id);
   }
 
+  @ApiBearerAuth()
+  @Roles(Role.Supervisor)                           // only supervisor can access
   @Get('overview')
   getOverview() {
     return this.supervisorService.getOverview();
