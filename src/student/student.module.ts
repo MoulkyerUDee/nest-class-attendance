fa35cca22ed0f392e1f17f1af6a6f0a276f29f52
aisha-abdulmanan <abdulmanan.sc123@s.msumain.edu.ph>
2025-05-17 18:23:20 +0800

    Update student module

--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { StudentService } from './student.service';
 import { StudentController } from './student.controller';
+import { Student } from './entities/student.entity';
+import { Class } from 'src/class/entities/class.entity'; 
+import { Attendance } from 'src/attendance/entities/attendance.entity';
 
 @Module({
+    imports: [
+    TypeOrmModule.forFeature([Student, Class, Attendance]),
+  ],
   controllers: [StudentController],
   providers: [StudentService],
+  //  exports: [StudentsService], 
 })
 export class StudentModule {}
