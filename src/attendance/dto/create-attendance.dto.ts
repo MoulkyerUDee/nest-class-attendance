ea7463f7b93ed1ce4f3c362ea2a45f61bafa3527
aisha-abdulmanan <abdulmanan.sc123@s.msumain.edu.ph>
2025-05-17 14:31:14 +0800

    Implement create / update attendance DTO

--- src/attendance/dto/create-attendance.dto.ts
+++ src/attendance/dto/create-attendance.dto.ts
@@ -1,1 +1,8 @@
-export class CreateAttendanceDto {}
+//For Student Self-Marking
+import { IsNotEmpty, IsUUID } from 'class-validator';
+
+export class CreateAttendanceDto {
+  @IsUUID()
+  @IsNotEmpty()
+  meetingId: string; // Only meetingId needed (user comes from auth token)
+}
--- src/attendance/dto/update-attendance.dto.ts
+++ src/attendance/dto/update-attendance.dto.ts
@@ -1,4 +1,14 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateAttendanceDto } from './create-attendance.dto';
+// For Teacher Confirmations
+import { IsEnum, IsOptional, IsString } from 'class-validator';
+import { AttendanceStatus } from 'src/enums/attendance-status.enum';
 
-export class UpdateAttendanceDto extends PartialType(CreateAttendanceDto) {}
+export class UpdateAttendanceDto {
+  @IsEnum(AttendanceStatus)
+  status: AttendanceStatus; // Teacher must set status
+
+  @IsOptional()
+  @IsString()
+  comments?: string; // Optional notes (teacher-only)
+}
+
+
